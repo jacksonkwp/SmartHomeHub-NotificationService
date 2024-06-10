71787070b71c78cf3639c91ac8a514e85727f417
Jackson Whiteley <44081344+jacksonkwp@users.noreply.github.com>
2024-06-09 17:26:26 -0700

    init

---
+++ NotificationService/src/test/java/com/smartHomeHub/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartHomeHub.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
